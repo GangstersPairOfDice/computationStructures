a43ad302ef39ed9510c4aff63c69b301ff930e54
GangstersPairOfDice <89167407+GangstersPairOfDice@users.noreply.github.com>
2023-08-23 21:16:21 +0200

    Added completed worksheet for unit 1

---
+++ Unit 1 - Basics of Information/coolNumber.py
@@ -0,0 +1,3 @@
+print("\n2^4096 = \n")
+print(2**4096)
+print("\n=\n\n1234 char's\n")
---
+++ Unit 1 - Basics of Information/encodeDecode.py
@@ -0,0 +1,17 @@
+A = "00"
+B = "01"
+C = "10"
+D = "11"
+
+encoded = D+A+B+A
+
+def decode(msg):
+
+	msgArray = ["test"]*(len(msg) / 2 )
+
+	for msg[::2] == "00" 
+
+	return msgArray
+
+
+print(decode(encoded))
